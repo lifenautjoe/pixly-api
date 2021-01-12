23bddf03f1cc9a5011b564cad44f33cb5a33d5bc
Joel Hernandez <joel@open-book.org>
2021-01-12 22:26:10 +0100

    :bug: add CORS config

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,31 @@
-import 'dotenv/config';
-import validateEnv from './utils/validateEnv';
-import 'reflect-metadata';
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import hpp from 'hpp';
-import morgan from 'morgan';
-import compression from 'compression';
-import errorMiddleware from './middlewares/error.middleware';
-import { logger, stream } from './utils/logger';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import PixlyService from './services/PixlyService';
+import "dotenv/config";
+import validateEnv from "./utils/validateEnv";
+import "reflect-metadata";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import morgan from "morgan";
+import compression from "compression";
+import errorMiddleware from "./middlewares/error.middleware";
+import { logger, stream } from "./utils/logger";
+import { Server as HttpServer } from "http";
+import { Server } from "socket.io";
+import { PixlyService } from "./services/PixlyService";
 
 validateEnv();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
 // Middleware
 
-if (env === 'production') {
-  app.use(morgan('combined', { stream }));
+if (env === "production") {
+  app.use(morgan("combined", { stream }));
   app.use(cors({ origin: true, credentials: true }));
-} else if (env === 'development') {
-  app.use(morgan('dev', { stream }));
+} else if (env === "development") {
+  app.use(morgan("dev", { stream }));
   app.use(cors({ origin: true, credentials: true }));
 }
 
@@ -45,11 +45,16 @@ app.use(errorMiddleware);
 
 // Start server
 
-app.listen(port, () => {
-  logger.info(`👾 Pixly is now listening on port ${port}`);
+const server = new HttpServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3006",
+    methods: ["GET", "POST"],
+  },
 });
 
-const server = createServer(app);
-const io = new Server(server);
-
 new PixlyService(io, logger);
+
+server.listen(port, () => {
+  logger.info(`👾 Pixly is now listening on port ${port}`);
+});
